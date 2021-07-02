bc0e39ddf4c59444eaa4ea8ea9b3e1657bca264f
xn <r2v_atr1n@mail.ru>
2021-07-02 18:12:21 +0600

    new slider and form state

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,65 +1,31 @@
-const formBtn = document.querySelector('.info__submit-btn')
-const addBtn = document.querySelector('.info__add-product')
-const mainState = document.querySelector('.info__beginning-pay-wrap')
-const addProdState = document.querySelector('.info__add-products-wrap')
-const prodState = document.querySelector('.info__product-cards-wrap')
+// wrappersState
+const defaultState = document.querySelector('.info__beginning-pay-wrap')
+const secondState = document.querySelector('.info__add-products-wrap')
+const productState = document.querySelector('.info__product-cards-wrap')
+
+// btnsState
+const defaultBtnState = document.querySelector('.info__add-product')
+const secondBtnState = document.querySelector('#secondBtnState')
+
+// radioBtns
+const radioBtn = document.querySelectorAll('.info__custom-radio')
+const changeBtn = document.querySelector('#changeValue')
 
 
-const textContent = document.getElementsByClassName('info__content-choose-product')
-const radioBox = document.getElementsByName('product')
+function redirectState() {
+    defaultBtnState.addEventListener('click', e => {
+        e.preventDefault()
 
-const navToggle = document.querySelector('#navToggle')
-const overlay = document.querySelector('.overlay')
-const body = document.querySelector('body')
+        defaultState.style.display = 'none'
+        secondState.style.display = 'block'
+    })
 
-navToggle.addEventListener('click', () => {
-    navToggle.classList.toggle('active')
-    overlay.classList.toggle('open')
-    body.classList.toggle('locked')
-})
+    secondBtnState.addEventListener('click', e => {
+        e.preventDefault()
 
-function radioChange() {
-    function testRadio() {
-        if (radioBox != null) {
-            
-        }
-    }
-
-    function checkRadio() {
-        let arr = radioBox
-        for (let i = 0; i < arr.length; i++) {
-            if (arr[i].checked) {
-                console.log('checked');
-                break
-            }
-        }
-    }
-
-    for (let i = 0; i < radioBox.length; i++) {
-        radioBox[i].onchange = testRadio
-    }   
-
-    checkRadio()
+        secondState.style.display = 'none'
+        productState.style.display = 'block'
+    })
 }
 
-
-document.addEventListener('DOMContentLoaded', () => {
-    addBtn.addEventListener('click', (e) => {
-        e.preventDefault()
-        
-        mainState.style.display = 'none'
-        addProdState.style.display = 'block'
-        formBtn.textContent = 'Continue'
-        // addProdState.style.display = 'none'
-        // prodState.style.display = 'block'
-    })
-
-    formBtn.addEventListener('click', (e) => {
-        e.preventDefault()
-
-        addProdState.style.display = 'none'
-        prodState.style.display = 'block'
-    })
-})
-    
-radioChange()
+redirectState()
--- dev/static/js/slider.js
+++ dev/static/js/slider.js
@@ -1,3 +1,13 @@
+const navToggle = document.querySelector('#navToggle')
+const overlay = document.querySelector('.overlay')
+const body = document.querySelector('body')
+
+navToggle.addEventListener('click', () => {
+    navToggle.classList.toggle('active')
+    overlay.classList.toggle('open')
+    body.classList.toggle('locked')
+})
+
 document.addEventListener('DOMContentLoaded', () => {
     const swiper = new Swiper('.swiper-container', {
         loop: true,
@@ -5,10 +15,6 @@ document.addEventListener('DOMContentLoaded', () => {
         spaceBetween: 30,
         slideToClickedSlide: true,
         watchOverflow: true,
-
-        autoplay: {
-            delay: 2000,
-        },
 
         pagination: {
             el: '.swiper-pagination',
@@ -18,6 +24,11 @@ document.addEventListener('DOMContentLoaded', () => {
         breakpoints: {
             1024: {
                 slidesPerView: 3,
+                spaceBetween: 20
+            },
+
+            768: {
+                slidesPerView: 2,
                 spaceBetween: 20
             },
 
